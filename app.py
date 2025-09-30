2a0f72523e384dd5537cfba2eac1b490cb291ccd
Bassouma-code <ibbou001@edu.cegepgarneau.ca>
2025-09-29 21:48:31 -0400

    Ajout page modification

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ regex_texte_court = re.compile(r"^[A-Za-zÀ-ÖØ-öø-ÿ\s]{1,50}$")
 regex_description=re.compile(r"^.{5,2000}$")
 regex_image=re.compile(r"^[A-Za-z0-9]{6,50}$")
 regex_monetaire=re.compile(r'^\d{1,3}(,\d{3})*([.,]\d{1,2})?$')
+regex_nombre = re.compile(r'^-?\d*\.?\d+$')
 
 app = Flask(__name__)
 logger = create_logger(app)
@@ -133,7 +134,7 @@ def confirmer():
 
 @app.route('/ajout', methods=["GET", "POST"])
 def ajout():
-    """Page qui permet d'ajouter ou modifier un service"""
+    """Page qui permet d'ajouter un service"""
 
     categories =[]
     photo_service="images/gratisography.jpg"
@@ -149,6 +150,7 @@ def ajout():
     classe_localisation= ""
     classe_description= ""
     classe_categorie= ""
+    classe_cout=""
     
 
     with bd.creer_connexion() as conn:
@@ -157,7 +159,7 @@ def ajout():
             for ligne in curseur.fetchall():
                 categories.append(ligne['nom_categorie'])
     
-    #Récupérer les valeurs des chamos du formulaire
+    #Récupérer les valeurs des champs du formulaire
     if  request.method == 'POST':
         titre=bleach.clean(request.form.get("titre", default="")).strip()
         localisation=bleach.clean(request.form.get("localisation", default="")).strip()
@@ -168,13 +170,10 @@ def ajout():
         categorie=request.form.get("categorie", default="")
 
         #validation du titre
-        # if not titre : 
-        #     abort(400, "Paramètre 'titre' manquant")
-      
         if not titre or not regex_texte_court.fullmatch(titre):
             erreur_titre = True
             classe_titre="is-invalid"
-            
+            # return abort(400)  
         else:
             classe_titre="is-valid"
         
@@ -182,6 +181,7 @@ def ajout():
         if not  localisation or not regex_texte_court.fullmatch(localisation):       
             erreur_localisation = True
             classe_localisation="is-invalid"
+            # return abort(400)
         else: 
             classe_localisation="is-valid"
 
@@ -189,17 +189,28 @@ def ajout():
         if not description or not regex_description.fullmatch(description):
             erreur_description= True
             classe_description="is-invalid"
+            # return abort(400)
         else:
             classe_description="is-valid"
+
+        #Validation du coût
+
+        if not regex_nombre.fullmatch(cout):
+            erreur_cout= True
+            classe_cout="is-invalid"
+            # return abort(400)
+        else:
+            classe_cout="is-valid"
 
         #validation de la catégorie
         if not categorie :
             erreur_categorie = True
             classe_categorie="is-invalid"
+            # return abort(400)
         else:
             classe_categorie="is-valid"
             
-        if not erreur_titre and not erreur_localisation and not erreur_description and not erreur_categorie:
+        if not erreur_titre and not erreur_localisation and not erreur_description and not erreur_cout and not erreur_categorie:
 
             with bd.creer_connexion() as conn:
                 with conn.get_curseur() as curseur:
@@ -235,26 +246,213 @@ def ajout():
             return redirect("/confirmation", code=303)
         else:
             # return redirect("/erreur_ajout", code=404)
-            return abort(400)
-   
-    return render_template('ajout.jinja',titre_page="Ajout/modification d'un service", 
+     
+            return render_template('ajout.jinja',titre_page="Ajout d'un service", 
                            categories=categories, 
                            classe_titre=classe_titre,
                            classe_localisation=classe_localisation,
                            classe_description=classe_description,
-                           classe_categorie=classe_categorie)
-
-
-
-@app.route('/erreur_ajout')
-def erreur_ajout():
-
-    """Affiche un message d'erreur si un détail de service inexistant"""
-    message_erreur="Détails d’un service inexistant."
-
-    #404
-    return render_template('base.jinja', message_erreur=message_erreur)
+                           classe_categorie=classe_categorie,
+                           classe_cout=classe_cout)
   
+    return render_template('ajout.jinja',titre_page="Ajout d'un service", 
+                           categories=categories, 
+                           classe_titre=classe_titre,
+                           classe_localisation=classe_localisation,
+                           classe_description=classe_description,
+                           classe_categorie=classe_categorie,
+                           classe_cout=classe_cout)
+
+@app.route('/modification', methods=["GET", "POST"])
+def modifier():
+
+    """Page qui permet de modifier un service"""
+
+    identifiant = request.args.get('id', type=int)
+ 
+    service = {}
+
+    # TODO : faire try except et mettre dans logger
+
+    with bd.creer_connexion() as conn:
+        with conn.get_curseur() as curseur:
+            curseur.execute(
+                '''
+            SELECT s.*, c.nom_categorie AS categorie_nom
+            FROM services s
+            LEFT JOIN categories c ON s.id_categorie = c.id_categorie
+            WHERE s.id_service = %(id_service)s
+            ''',
+            {'id_service': identifiant}
+            )
+            service = curseur.fetchone()
+          
+    categories =[]
+    photo_service="images/gratisography.jpg"
+
+
+    titre_service=service['titre']
+    localisation_service=service['localisation']
+    description_service=service['description']
+    cout_service=service['cout']
+    date_cr_service=service['date_creation']
+    statut_service=service['actif']
+ 
+    id_categorie_service=service['id_categorie']
+    
+
+    with bd.creer_connexion() as conn:
+        with conn.get_curseur() as curseur:
+            curseur.execute('SELECT nom_categorie FROM categories WHERE id_categorie =%(id_c)s',
+                            {'id_c':id_categorie_service }
+                            
+                            )
+            nom_categorie_service=curseur.fetchone()['nom_categorie']
+            
+            
+    #         for ligne in curseur.fetchall():
+    #             categories.append(ligne['nom_categorie'])
+    
+    #    "SELECT id_categorie FROM categories WHERE nom_categorie = %(nom)s",
+    #                     {'nom': categorie}
+    #                 )
+    #                 id_categorie = curseur.fetchone()['id_categorie']
+    erreur_titre = False
+    erreur_localisation = False
+    erreur_description = False
+    erreur_categorie = False
+    erreur_cout = False
+
+    classe_titre= ""
+    classe_localisation= ""
+    classe_description= ""
+    classe_cout= ""
+
+    
+    with bd.creer_connexion() as conn:
+        with conn.get_curseur() as curseur:
+            curseur.execute('SELECT nom_categorie FROM categories ORDER BY nom_categorie')
+            for ligne in curseur.fetchall():
+                categories.append(ligne['nom_categorie'])
+                
+    
+    #Récupérer les valeurs des champs du formulaire
+    if  request.method == 'POST':
+        titre=bleach.clean(request.form.get("titre", default="")).strip()
+        localisation=bleach.clean(request.form.get("localisation", default="")).strip()
+        description=bleach.clean(request.form.get("description", default="")).strip()
+        cout=request.form.get("cout", type=float, default=0.0)
+        st=request.form.get("statut")
+        statut = bool(st)
+        # categorie=request.form.get("categorie", default="")
+
+
+        # if not titre : 
+        #     abort(400, "Paramètre 'titre' manquant")
+
+        #validation du titre
+        if not titre or not regex_texte_court.fullmatch(titre):
+            erreur_titre = True
+            classe_titre="is-invalid"
+            return abort(400)
+            
+        else:
+            classe_titre="is-valid"
+        
+        #validation de la localisation
+        if not  localisation or not regex_texte_court.fullmatch(localisation):       
+            erreur_localisation = True
+            classe_localisation="is-invalid"
+            return abort(400)
+        else: 
+            classe_localisation="is-valid"
+
+              #Validation du coût
+
+        if not regex_nombre.fullmatch(cout):
+            erreur_cout= True
+            classe_cout="is-invalid"
+            return abort(400)
+        else:
+            classe_cout="is-valid"
+
+        #validation de la description
+        if not description or not regex_description.fullmatch(description):
+            erreur_description= True
+            classe_description="is-invalid"
+            return abort(400)
+        else:
+            classe_description="is-valid"
+
+            
+        if not erreur_titre and not erreur_localisation and not erreur_description and not erreur_cout and not erreur_categorie:
+
+            with bd.creer_connexion() as conn:
+                with conn.get_curseur() as curseur:
+                    # Récupérer l'id_categorie correspondant au nom
+                    # curseur.execute(
+                    # "SELECT id_categorie FROM categories WHERE nom_categorie = %(nom)s",
+                    #     {'nom': categorie}
+                    # )
+                    # id_categorie = curseur.fetchone()['id_categorie']
+
+                    curseur.execute(
+                        """
+                        UPDATE services 
+                        SET titre = %(titre)s,
+                            description = %(description)s,
+                            localisation = %(localisation)s,
+                            cout = %(cout)s,
+                            actif = %(actif)s
+                        WHERE id_service = %(id_service)s
+                        """,
+                        {
+                            'titre': titre,
+                            'description': description,
+                            'localisation': localisation,
+                            'cout': cout,
+                            'actif': statut,
+                            'id_service': identifiant 
+                        }
+)
+                      
+            return redirect("/confirmation", code=303)
+        else:
+            # return redirect("/erreur_ajout", code=404)
+     
+            return render_template('modification.jinja',titre_page="Modification d'un service", 
+                           categories=categories, 
+                           titre=titre_service,
+                           localisation=localisation_service,
+                           description=description_service,
+                           cout=cout_service,
+                           date_creation=date_cr_service,
+                           statut=statut_service,
+                           categorie_service=nom_categorie_service,
+
+                           classe_titre=classe_titre,
+                           classe_localisation=classe_localisation,
+                           classe_description=classe_description,
+                           classe_cout=classe_cout
+                           )
+
+  
+    return render_template('modification.jinja',titre_page="Modification d'un service", 
+                            categories=categories, 
+                            titre=titre_service,
+                            localisation=localisation_service,
+                            description=description_service,
+                            cout=cout_service,
+                            date_creation=date_cr_service,
+                            statut=statut_service,
+                            categorie_service=nom_categorie_service,
+
+                            classe_titre=classe_titre,
+                            classe_localisation=classe_localisation,
+                            classe_description=classe_description,
+                            classe_cout=classe_cout
+                           )
+
 @app.route('/nos-services')
 def lister_service():
     """Affiche la liste de tous les services proposés par les utilisateurs du site"""
@@ -310,5 +508,7 @@ def erreur_interne(erreur):
         ), 500
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
